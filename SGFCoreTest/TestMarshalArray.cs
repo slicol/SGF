d7b400f71a70c8f17190e40c97d2b18d04a17c47
Slicol <slicol@qq.com>
2018-06-26 12:50:36 +0800

    提交SGFCoreTest工程
    Signed-off-by: Slicol <slicol@qq.com>

---
+++ SGFCoreTest/TestMarshalArray.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SGF.G3Lite;
+using SGF.Marshals;
+
+namespace SGFCoreTest
+{
+    [TestClass]
+    public class TestMarshalArray
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            MarshalArrayBase.Cleanup();
+        }
+
+
+        [TestMethod]
+        public void TestSetterGetterInt()
+        {
+            int count = 100;
+            MarshalArray<int> list = new MarshalArray<int>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = i;
+            }
+
+            Assert.AreEqual(list.Length, count);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(list[i], i);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestSetterGetterFloat()
+        {
+            int count = 100;
+            MarshalArray<float> list = new MarshalArray<float>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = i;
+            }
+
+            Assert.AreEqual(list.Length, count);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(list[i], i);
+            }
+        }
+
+        [TestMethod]
+        public void TestSetterGetterVector()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+            Assert.AreEqual(list.Length, count);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(list[i], new Vector3(i, i, i));
+            }
+        }
+
+        [TestMethod]
+        public void TestIndexOf()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+            int result = -1;
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 0, list.Length);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 5, list.Length);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 6, list.Length);
+            Assert.AreEqual(result, -1);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 5, 1);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 5, 0);
+            Assert.AreEqual(result, -1);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 0, 6);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.IndexOf(list, new Vector3(5, 5, 5), 0, 3);
+            Assert.AreEqual(result, -1);
+        }
+
+
+        [TestMethod]
+        public void TestLastIndexOf()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+            int result = -1;
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 0, list.Length);
+            Assert.AreEqual(result, -1);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 5, list.Length);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 6, list.Length);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 5, 1);
+            Assert.AreEqual(result, 5);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 5, 0);
+            Assert.AreEqual(result, -1);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 0, 6);
+            Assert.AreEqual(result, -1);
+
+            result = MarshalArrayBase.LastIndexOf(list, new Vector3(5, 5, 5), 0, 3);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void TestCopyMarshal2Marshal()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+            MarshalArray<Vector3> list2 = new MarshalArray<Vector3>(count);
+            MarshalArrayBase.Copy(list, 0, list2, 0, count);
+            Assert.AreEqual(list2.Length, count);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(list2[i], new Vector3(i, i, i));
+            }
+
+
+            MarshalArray<Vector3> list3 = new MarshalArray<Vector3>(10);
+            MarshalArrayBase.Copy(list2, 10, list3, 5, 10);
+            Assert.AreEqual(list3[5], new Vector3(10, 10, 10));
+            Assert.AreEqual(list3[9], new Vector3(14, 14, 14));
+        }
+
+        [TestMethod]
+        public void TestCopyManaged2Marshal()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+
+            Vector3[] list2 = new Vector3[count];
+            MarshalArrayBase.Copy(list, 0, list2, 0, count);
+            Assert.AreEqual(list2.Length, count);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.AreEqual(list2[i], new Vector3(i, i, i));
+            }
+
+
+            MarshalArray<Vector3> list3 = new MarshalArray<Vector3>(10);
+            MarshalArrayBase.Copy(list2, 10, list3, 5, 10);
+            Assert.AreEqual(list3[5], new Vector3(10, 10, 10));
+            Assert.AreEqual(list3[9], new Vector3(14, 14, 14));
+        }
+
+        [TestMethod]
+        public void TestCopyIntPtr2IntPtr()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+
+            MarshalArray<Vector3> list2 = new MarshalArray<Vector3>(count);
+            MarshalArrayBase.CopyBytes(list.Ptr, list.ElementSize, list2.Ptr, 0, list.ByteLength);
+            Assert.AreEqual(list2.Length, count);
+            for (int i = 0; i < count - 1; i++)
+            {
+                Assert.AreEqual(list2[i], new Vector3(i + 1, i + 1, i + 1));
+            }
+        }
+
+
+        [TestMethod]
+        public void TestForEach()
+        {
+            int count = 100;
+            MarshalArray<Vector3> list = new MarshalArray<Vector3>(count);
+            Assert.AreEqual(list.Length, count);
+
+            for (int i = 0; i < count; i++)
+            {
+                list[i] = new Vector3(i, i, i);
+            }
+
+            int j = 0;
+            foreach (var vector3 in list)
+            {
+                Assert.AreEqual(vector3, new Vector3(j, j, j));
+                j++;
+            }
+
+        }
+    }
+}
---
+++ SGFCoreTest/TestMarshalList.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SGF.Marshals;
+
+namespace SGFCoreTest
+{
+    [TestClass]
+    public class TestMarshalList
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            MarshalArrayBase.Cleanup();
+        }
+
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            Assert.AreEqual(list.Capacity, 4);
+            Assert.AreEqual(list.Count, 0);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            Assert.AreEqual(list.Count, 10);
+            Assert.AreEqual(list.Capacity, 16);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(list[i], i);
+            }
+        }
+
+        [TestMethod]
+        public void TestAddRange()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            MarshalList<int> list2 = new MarshalList<int>();
+            list2.AddRange(list);
+            Assert.AreEqual(list2.Count, 100);
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(list2[i], i);
+            }
+
+            list2.AddRange(list);
+            Assert.AreEqual(list2.Count, 200);
+            for (int i = 100; i < 200; i++)
+            {
+                Assert.AreEqual(list2[i], i - 100);
+            }
+
+            list2.AddRange(list2);
+            Assert.AreEqual(list2.Count, 400);
+            for (int i = 200; i < 300; i++)
+            {
+                Assert.AreEqual(list2[i], i - 200);
+            }
+
+            int[] list3 = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                list3[i] = 10000 + i;
+            }
+            list.AddRange(list3);
+            for (int i = 100; i < 200; i++)
+            {
+                Assert.AreEqual(list[i], 10000 + i - 100);
+            }
+
+
+            List<int> list4 = new List<int>();
+            list4.AddRange(list3);
+            list.AddRange(list4);
+            for (int i = 200; i < 300; i++)
+            {
+                Assert.AreEqual(list[i], 10000 + i - 200);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Insert(100, 10001);
+            list.Insert(100, 10002);
+            Assert.AreEqual(list.Count, 102);
+            Assert.AreEqual(list[100], 10002);
+            Assert.AreEqual(list[101], 10001);
+
+            list.Insert(0, 10003);
+            Assert.AreEqual(list[0], 10003);
+            Assert.AreEqual(list[1], 0);
+            Assert.AreEqual(list[2], 1);
+            Assert.AreEqual(list[99], 98);
+            Assert.AreEqual(list[102], 10001);
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            Assert.AreEqual(list.Remove(2), true);
+            Assert.AreEqual(list.Count, 99);
+            Assert.AreEqual(list[2], 3);
+
+            Assert.AreEqual(list.Remove(2), false);
+
+            list.RemoveAt(0);
+            Assert.AreEqual(list[0], 1);
+            list.RemoveAt(97);
+            Assert.AreEqual(list.Count, 97);
+
+            list.RemoveRange(0, 96);
+            Assert.AreEqual(list.Count, 1);
+            Assert.AreEqual(list[0], 98);
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            Assert.AreEqual(list.Contains(5), true);
+            Assert.AreEqual(list.Contains(500), false);
+        }
+
+        [TestMethod]
+        public void TestCopyToManagedArray()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            int[] list1 = new int[100];
+            list.CopyTo((Array)list1, 0);
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(list1[i], i);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestForEach()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            int j = 0;
+            foreach (var i in list)
+            {
+                Assert.AreEqual(i, j);
+                ++j;
+            }
+        }
+
+        [TestMethod]
+        public void TestGetRange()
+        {
+            MarshalList<int> list = new MarshalList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            var list1 = list.GetRange(50, 50);
+            Assert.AreEqual(list1.Count, 50);
+            for (int i = 0; i < 50; i++)
+            {
+                Assert.AreEqual(list1[i], i + 50);
+            }
+
+        }
+
+
+
+
+
+    }
+}
