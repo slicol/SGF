807c9d73326a47e54e3803f0e310d2030b8995c6
slicol <slicol@qq.com>
2019-10-17 01:51:25 +0800

    Modify

--- Libs/PublishLibrary.bat
+++ Libs/PublishLibrary.bat
@@ -2,5 +2,5 @@ set CurDir=%~dp0
 set LibName=%1
 echo LibName=%LibName%
 pdb2mdb %LibName%.dll
-xcopy %LibName%.dll %CurDir%..\..\SpiceUnity\SpiceUnity\SpiceUnityApp\Assets\Plugins\ManagedLibs\  /y
-xcopy %LibName%.dll.mdb %CurDir%..\..\SpiceUnity\SpiceUnity\SpiceUnityApp\Assets\Plugins\ManagedLibs\  /y
+xcopy %LibName%.dll %CurDir%..\..\SpiceUnity\SpiceUnity\LinkGameApp\Assets\Plugins\ManagedLibs\  /y
+xcopy %LibName%.dll.mdb %CurDir%..\..\SpiceUnity\SpiceUnity\LinkGameApp\Assets\Plugins\ManagedLibs\  /y
